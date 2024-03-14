fef94779ed230e7e77ef0305520f863d6ecaa773
Raaja Kartikeya <raaja@Raajas-MacBook-Air.local>
2024-03-14 20:45:32 +0530

    third commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var n = 1
+var a = document.getElementById("brand")
+var b = document.getElementById("model")
+var c = document.getElementById("cc")
+var d = document.getElementById("price")
+var tab = document.getElementById("table-container")
+function addData()
+    {
+        if(a.value && b.value && c.value && d.value)
+        {
+        var result = "<td>" + n + "</td>" + "<td>" + a.value + "</td>" + "<td>" + b.value + "</td>" + "<td>" + c.value + "</td>" + "<td>" + d.value + "</td>" + "<td><button id=\"deldata\" onclick =\"delrow(event)\">Delete</button></td>"
+        console.log(result)
+        var trow = document.createElement("tr")
+        trow.innerHTML = result
+        tab.insertAdjacentElement("beforeend",trow)
+        n = n + 1;
+        }
+        else
+        {
+            alert("Enter All Details!")
+        }
+    }
+function delrow(event)
+    {
+        event.target.parentElement.parentElement.remove()
+    }
